983da99d72699c2943818c87304a7fdde9b1fb97
Yurii Sorokin <yurii_sorokin@epam.com>
2014-05-29 14:15:28 +0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    watch: {
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', []);
+
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# crxde-pipe
